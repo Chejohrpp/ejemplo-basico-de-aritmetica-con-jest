7de7466a3ae0662bc326f079ee9914d4bf53e22f
Chejohrpp <sergioroherp@gmail.com>
2023-03-28 18:24:47 -0600

    test de fallo

--- test/math.test.js
+++ test/math.test.js
@@ -7,3 +7,8 @@ test('add 1 + 2 igual a 3', () => {
 test('add -1 + 1 va ser igual a 0', () => {
   expect(sum(-1, 1)).toBe(0);
 });
+
+// test que falla
+// test('add 5 + 7 va a ser 9, Debe fallar', () => {
+//   expect(sum(5, 7)).toBe(9);
+// });
